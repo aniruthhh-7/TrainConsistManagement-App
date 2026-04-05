8e81a1ae1bdbd76895f4a8b4c7d016b2e7786f37
aniruthhh-7 <torque.torricelli@gmail.com>
2026-04-05 10:01:50 +0530

    UC6 Completed

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println(" ======================================= ");
